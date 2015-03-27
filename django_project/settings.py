cb1bdd37da9f99e92b93f0cba1946b0be67fb45b
abomariam <abo_mariam1@hotmail.com>
2015-03-27 22:36:32 +0200

    my django app

--- django_project/settings.py
+++ django_project/settings.py
@@ -19,13 +19,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '_t208@$3e@i(qbe!--1c-^f3d(3p#w%lckw5=1k$zin^mt&)p#'
 
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+STATIC_ROOT = 'staticfiles'
 
 # Application definition
 
@@ -62,12 +66,8 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES['default'] = dj_database_url.config()
+    
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -93,3 +93,7 @@ TEMPLATE_DIRS = (
 )
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
