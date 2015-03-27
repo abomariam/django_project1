358f0317ba8dd82558f76a0782469e099444d69d
abomariam <abo_mariam1@hotmail.com>
2015-03-27 22:46:39 +0200

    correct database

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 
 # Application definition
 
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config()
-    
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}    
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
